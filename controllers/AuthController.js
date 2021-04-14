35e5b0deea88a5bcfe69ed8be77152eb12586c3e
SdRohan <sandrorohan@hotmail.com>
2021-04-14 20:47:05 -0300

    Fix de método para login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,23 +5,24 @@ class AuthController {
    * Rota: "/login"
    * Método: POST
    */
-  login(req, res) {
+   login(req, res) {
     const user = req.body.user;
     const password = req.body.password;
 
     if(user == "" || password == "") return false;
 
-    usuarios.forEach(usuario => {
-      if(user == usuario.user && password == usuario.password) {
-        req.session.user = {
-          user: user,
-          password: password
-        }
-        return res.redirect("/home");        
-      }
-    }) 
+    const userMatches = usuarios.find(usuario => usuario.user == user && usuario.password == password);
 
-    
+    if(userMatches) {
+      req.session.user = {
+        user: user,
+        password: password,
+      };
+      
+      return res.redirect("/home");
+    } else {
+      return res.redirect("/");
+    }
   }
 }
 
